5b61f416cd55c2f6345bae7755aec7ea09f601bf
Ed McAdams <eatdigital@outlook.com>
2018-12-05 13:20:13 -0500

    add day3 skeleton

---
+++ AdventOfCode2018/AdventOfCode2018/Day3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode2018
+{
+    public partial class Day3
+    {
+        [Fact]
+        public void Part1()
+        {
+            #region // Arrange
+
+            string exampleInput1 = "";
+
+            #endregion
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            #region // Arrange
+
+            string exampleInput1 = "";
+
+            #endregion
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ AdventOfCode2018/AdventOfCode2018/Day3PuzzleInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2018
+{
+    public partial class Day3
+    {
+        #region _puzzleInput
+
+        private readonly string _puzzleInput = @"";
+
+        #endregion
+    }
+}
