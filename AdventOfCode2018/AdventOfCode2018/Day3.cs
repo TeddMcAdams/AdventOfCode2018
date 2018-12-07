66827f5d300499e0efa41d79d112007465952a4b
Ed McAdams <teddmcadams@outlook.com>
2018-12-06 22:11:28 -0500

    day 3 part 1

--- AdventOfCode2018/AdventOfCode2018/Day3.cs
+++ AdventOfCode2018/AdventOfCode2018/Day3.cs
@@ -1,7 +1,8 @@
-﻿using AdventOfCode2018.Models;
+﻿using AdventOfCode2018.Helpers;
+using AdventOfCode2018.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Xunit;
 
 namespace AdventOfCode2018
@@ -21,9 +22,11 @@ namespace AdventOfCode2018
 
             // Act
             int actual1 = CalculateSquareInchesOfOverlappedFabric(exampleInput1);
+            int actual = CalculateSquareInchesOfOverlappedFabric(_puzzleInput);
 
             // Assert
             Assert.Equal(4, actual1);
+            Assert.Equal(105231, actual);
         }
 
         [Fact]
@@ -38,17 +41,56 @@ namespace AdventOfCode2018
             // Act
 
             // Assert
+            Assert.True(false);
         }
 
         private int CalculateSquareInchesOfOverlappedFabric(string input)
         {
-            int squareInches = 0;
-
             var coordinateMap = new Dictionary<Coordinate, int>();
 
-            // find coordinate and increment value
+            string[] elfFabricClaims = Parsing.SplitOnNewLine(input);
 
-            return squareInches;
+            foreach (string claim in elfFabricClaims)
+            {
+                List<Coordinate> claimCoordinates = ConvertElfsFabricClaimToCoordinates(claim);
+
+                foreach (Coordinate coordinate in claimCoordinates)
+                {
+                    if (coordinateMap.ContainsKey(coordinate))
+                        coordinateMap[coordinate]++;
+                    else
+                        coordinateMap.Add(coordinate, 1);
+                }
+            }
+
+            return coordinateMap.Where(kvp => kvp.Value > 1).Count(); ;
+        }
+
+        private List<Coordinate> ConvertElfsFabricClaimToCoordinates(string claim)
+        {
+            var coordinates = new List<Coordinate>();
+
+            string[] delimitedValues = claim.Split(new[] { " @ ", ": " }, StringSplitOptions.None);
+
+            string claimId = delimitedValues[0];
+            string[] positionInfo = delimitedValues[1].Split(',');
+            string[] sizeInfo = delimitedValues[2].Split('x');
+
+            int cornerCoordinateX = int.Parse(positionInfo[0]) + 1;
+            int cornerCoordinateY = int.Parse(positionInfo[1]) + 1;
+
+            int width = int.Parse(sizeInfo[0]);
+            int height = int.Parse(sizeInfo[1]);
+
+            for (int i = cornerCoordinateX; i < cornerCoordinateX + width; i++)
+            {
+                for (int j = cornerCoordinateY; j < cornerCoordinateY + height; j++)
+                {
+                    coordinates.Add(new Coordinate { X = i, Y = j });
+                }
+            }
+
+            return coordinates;
         }
 
     }
